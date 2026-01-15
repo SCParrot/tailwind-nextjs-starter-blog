5c18c8137d8326a3fdc1ab456c2006b415d1846b
parrotace <parrotace@outlook.com>
2026-01-15 20:32:02 +0800

    modify personal info

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,6 +7,12 @@ interface Project {
 
 const projectsData: Project[] = [
   {
+    title: 'saas-template',
+    description: `A free Next.js landing page template designed to showcase open source projects, SaaS products, online services, and more.`,
+    imgSrc: '/static/images/logo.png',
+    href: 'https://github.com/SCParrot/saas-template',
+  },
+  {
     title: 'n8n execution monitor',
     description: `A workflow execution tracking panel built with Next.js + ECharts + Tailwind CSS for visualizing N8N workflow execution status.`,
     imgSrc: '/static/images/logo.png',
